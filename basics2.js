509844648f9e7cf725fb11375a2ff6735f48914c
Mai Aloni <maia@tingz.co.il>
2020-10-07 15:48:54 +0300

    added exes to basics2 15:46

--- basics2.js
+++ basics2.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* Q1 */
-//Write a function that gets 2 numbers and return their sum
+// Write a function that gets 2 numbers and return their sum
 function calcSum(num1, num2) {
   if (typeof num1 === "number" && typeof num2 === "number") {
     return num1 + num2;
@@ -15,7 +15,7 @@ console.log("Sum of " + num1 + " + " + num2 + " is " + sum);
 
 /* Q2 */
 /* Write a function isEven that gets a number, and returns true if the number is
-even otherwise false*/
+even otherwise false */
 function isEven(num) {
   return num % 2 === 0;
 }
@@ -26,7 +26,7 @@ console.log(isEvenRes);
 console.log(isEvenRes2);
 
 /* Q3 */
-/* Write a function getBigger that gets 2 numbers and returns the bigger one*/
+/* Write a function getBigger that gets 2 numbers and returns the bigger one */
 function getBigger(num1, num2) {
   let biggerNum = num1;
   if (num2 > num1) biggerNum = num2;
@@ -38,8 +38,8 @@ console.log(getBigger(6, 10));
 console.log(getBigger(-6, 0));
 
 /* Q4 */
-/*Write the function myPow that gets 2 params: base, exp and
-returns the power*/
+/* Write the function myPow that gets 2 params: base, exp and
+returns the power */
 function calcPow(base, exp) {
   let multi = 1;
   if (exp < 0) {
@@ -52,7 +52,7 @@ function calcPow(base, exp) {
   return multi;
 }
 
-//assuming positive number
+// assuming positive number
 function calcPowRecursion(base, exp) {
   if (exp != 0) return base * calcPowRecursion(base, exp - 1);
   return 1;
@@ -68,14 +68,87 @@ console.log(
 );
 
 /* Q5 */
-/*Write the function factorial that gets a number n and return n!*/
+/* Write the function factorial that gets a number n and return n! */
 function calcFactorial(num) {
-    if (num < 0) 
-        return -1;
-    else if (num == 0) 
-      return 1;
-    else 
-        return (num * calcFactorial(num - 1));
+  if (num < 0) return -1;
+  else if (num == 0) return 1;
+  else return num * calcFactorial(num - 1);
 }
 
 console.log(calcFactorial(3));
+
+/* Q6 */
+// Write A function getRandomInteger(min, max) get that generates a random integer between min and max.
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log(
+  "The random number should be between 10 to 20. The result from the function is: ",
+  getRandomInteger(10, 20)
+);
+console.log(
+  "The random number should be between -5 to 5. The result from the function is: ",
+  getRandomInteger(-5, 5)
+);
+console.log(
+  "The random number should be between 4 to 1000. The result from the function is: ",
+  getRandomInteger(4, 1000)
+);
+
+/* Q7 */
+/* Write a program that generates 10 random numbers. 
+The numbers should be generated so each number is greater than the ones generated before. 
+To simplify, generate the first number n so it is between (0→1000), and each
+subsequent number will be in the range of ( n → n+1000) */
+
+var rndNum1 = getRandomInteger(0, 1000);
+var rndNum2 = 0;
+var count = 0;
+console.log("The first number in range 0 to 1000 ", rndNum1);
+console.log();
+
+while (count < 10) {
+  rndNum2 = getRandomInteger(rndNum1, rndNum2 + 1000);
+  console.log("The random number is: ", rndNum1);
+  count++;
+  rndNum1 = rndNum2;
+}
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/* Q8 */
+/* Write a function getAsterisks(length) that returns a string containing
+asterisks according the number supplied.
+for example: when the requested length is 4, it returns '****' */
+function getAsterisks(length) {
+  let strAsterisks = "";
+  for (let i = 0; i < length; i++) {
+    strAsterisks += "*";
+  }
+  return strAsterisks;
+}
+
+console.log(getAsterisks(4));
+
+/* Q9 */
+/* Write a function: drawTriangle(height) that prints a triangle:
+(the requested height here is 4)
+Hint: use the function getAsterisks in a loop. */
+function drawTriangle(height) {
+    let str = "";
+    for (var i = 1; i <= height; i++)
+    {
+      str += getAsterisks(i) + '\n';
+    }
+    
+    for (var i = height - 1; i > 0; i--)
+    {
+      str += getAsterisks(i) + '\n';
+    }
+    console.log(str);
+}
+
+drawTriangle(4);
